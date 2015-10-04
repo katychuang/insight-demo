ed240c2610a53bf96c89c043f8d15728150b807c
katychuang <katychuang@gmail.com>
2015-10-04 22:58:14 +0000

    tidy up example scripts

--- scripts/spark_stream.py
+++ scripts/spark_stream.py
@@ -1,17 +1,20 @@
-
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 from settings import *
 import sys
 
-MASTER = "spark://{}:7077".format(settings.MASTER_SPARK)
-PUBLIC_IP = "{}:2181".format(settings.MASTER_PUBLIC_IP)
+# print settings.MASTER_SPARK
+
+# MASTER = "spark://{}:7077".format(settings.MASTER_SPARK)
+# PUBLIC_IP = "{}:2181".format(settings.MASTER_PUBLIC_IP)
 topic = sys.argv[1]
 partitions = sys.argv[2]
+MASTER = "spark://{}:7077".format("ec2-54-173-157-255.compute-1.amazonaws.com")
+PUBLIC_IP = "{}:2181".format(sys.argv[3])
 
 # Create a local StreamingContext with two working thread and batch interval of 2 second
-sc = SparkContext(MASTER, "MyTest")
+sc = SparkContext(MASTER, "MyTest1")
 ssc = StreamingContext(sc, 1)
 
 kafkaStream = KafkaUtils.createStream(ssc, PUBLIC_IP, "flask", {topic: partitions})
